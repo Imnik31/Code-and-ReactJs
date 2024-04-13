3c7913a37d911bf73c430e55f6137aa3655bb780
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-04-13 19:32:50 +0530

    add props in component

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -6,7 +6,7 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
-  let myObj = {
+  let myObj = {                             // define an object
     username: "hitesh",
     age: 21
   }
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="chaiaurcode" btnText="click me" />           
       <Card username="hitesh" />
     </>
   )
